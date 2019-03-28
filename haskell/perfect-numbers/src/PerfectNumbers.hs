fbd470b8581abdf3f4c92358743c46ff43846b37
Saxon <saxon.jensen@gmail.com>
2019-03-28 22:55:16 +0800

    Finish perfect numbers
    Finish perfect numbers

--- haskell/perfect-numbers/src/PerfectNumbers.hs
+++ haskell/perfect-numbers/src/PerfectNumbers.hs
@@ -2,5 +2,14 @@ module PerfectNumbers (classify, Classification(..)) where
 
 data Classification = Deficient | Perfect | Abundant deriving (Eq, Show)
 
+factors :: Int -> [Int]
+factors n = [x | x <- [1..n-1], n `mod` x == 0]
+
 classify :: Int -> Maybe Classification
-classify = error "You need to implement this function."
+classify x
+    | x <= 0 = Nothing
+    | s == x = Just Perfect
+    | s < x = Just Deficient
+    | otherwise = Just Abundant
+        where
+            s = sum $ factors x
