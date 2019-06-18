b1e90fa2ff9e70b1847d006c7259c682725ca222
Saxon Jensen <saxon.jensen@healthengine.com.au>
2019-06-18 09:55:39 +0800

    Refactor collatz

--- haskell/collatz-conjecture/src/CollatzConjecture.hs
+++ haskell/collatz-conjecture/src/CollatzConjecture.hs
@@ -1,12 +1,8 @@
 module CollatzConjecture (collatz) where
 
-collatzSequence :: Integer -> [Integer]
-collatzSequence 1 = []
-collatzSequence x = v : collatzSequence v
-    where
-        v = if even x then x `div` 2 else 3 * x + 1
-
 collatz :: Integer -> Maybe Integer
 collatz n
     | n <= 0 = Nothing
-    | otherwise = Just $ toInteger . length $ collatzSequence n
+    | otherwise = Just $ toInteger . length . (takeWhile (/= 1)) $ iterate coll n
+        where
+            coll x = if even x then x `div` 2 else 3 * x + 1
