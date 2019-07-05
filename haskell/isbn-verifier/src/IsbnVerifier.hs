6ff57488742e9a9c2bd0089163ceeb0fad5afeab
Saxon Jensen <saxon.jensen@healthengine.com.au>
2019-07-05 11:11:50 +0800

    Refactor isbn

--- haskell/isbn-verifier/src/IsbnVerifier.hs
+++ haskell/isbn-verifier/src/IsbnVerifier.hs
@@ -8,15 +8,17 @@ validChar c = (c == 'X') || isNumber c
 toIntList :: String -> [Int]
 toIntList = map conv
     where
-        conv c = if isNumber c then digitToInt c else 10
+        conv c
+            | isNumber c = digitToInt c
+            | otherwise = 10
 
-stringValid :: String -> Bool
-stringValid xs = length xs == 10 && notElem 'X' (take 9 xs)
+xInTenth :: String -> Bool
+xInTenth = (notElem 'X') . (take 9)
 
-isValid :: String -> Bool
-isValid cs = foldl (\c (i,n) -> c + i * n) 0 (zip [10,9..1] $ toIntList cs) `mod` 11 == 0
+validSum :: String -> Bool
+validSum cs = (sum $ zipWith (*) [10,9..1] $ toIntList cs) `mod` 11 == 0
 
 isbn :: String -> Bool
-isbn s = stringValid filtered && isValid filtered
+isbn s = length validChars == 10 && xInTenth validChars && validSum validChars
     where
-        filtered = filter validChar s
+        validChars = filter validChar s
